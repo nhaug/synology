7db006ea6ace05cae10c57376395b7286bda0928
plzPayAttention <mail@n-haug.de>
2016-10-13 19:45:56 +0200

    Update and rename sammle_dateien_4_nils.bash to collectImportantFilesAndData.bash
    Translate Comments in Engl.

--- collectImportantFilesAndData.bash
+++ collectImportantFilesAndData.bash
@@ -5,7 +5,7 @@
 file_httpd_ps_out="/tmp/httpd_prozesse_$(date +'%Y%m%d')"
 ps | grep httpd > $file_httpd_ps_out
 
-# TAR-Archiv mit allen wichtigen Dateien erstellen:
+# TAR-Archiv from all important files:
 files_2_tar="${file_httpd_ps_out} /etc/group /etc/passwd /etc/httpd/conf/httpd* /usr/syno/etc/rc.sysv/httpd-user-conf-writer.sh /usr/syno/sbin/synoservicecfg /usr/bin/httpd"
 
 tar -czvf /tmp/files_4_nils_$(date +"%Y%M%d").tar.gz $files_2_tar 
