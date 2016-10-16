1719edef5b6ff117f81b39863ebe7b0e73bde018
plzPayAttention <mail@n-haug.de>
2016-10-16 19:52:24 +0200

    Update comments @collectImportantFilesAndData.sh

--- collectImportantFilesAndData.bash
+++ collectImportantFilesAndData.bash
@@ -1,11 +1,11 @@
 #!/usr/syno/synoha/bin/bash
 
-# Erstmal den Output von httpd in temp. Datei
-
+# Collect Output of temporary HTTP Output
 file_httpd_ps_out="/tmp/httpd_prozesse_$(date +'%Y%m%d')"
 ps | grep httpd > $file_httpd_ps_out
 
-# TAR-Archiv from all important files:
+# All files and dirs that we would like to backup:
 files_2_tar="${file_httpd_ps_out} /etc/group /etc/passwd /etc/httpd/conf/httpd* /usr/syno/etc/rc.sysv/httpd-user-conf-writer.sh /usr/syno/sbin/synoservicecfg /usr/bin/httpd"
 
+# Create tar from all important files:
 tar -czvf /tmp/files_4_nils_$(date +"%Y%M%d").tar.gz $files_2_tar 
