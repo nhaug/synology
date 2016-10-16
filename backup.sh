2a85153f16e6004ef8400fa13466f57bdd62b69d
plzPayAttention <mail@n-haug.de>
2016-10-16 19:49:15 +0200

    Update backup.sh
    Update to english Language and change some echos to show more meanigful output. Also changed variables from german to english:
    datum -> date
    zeile -> line

--- backup.sh
+++ backup.sh
@@ -10,8 +10,8 @@ set -u
 #
 ###############################################################################
 
-# Get Datum von heute
-datum=$(/bin/date +'%Y%m%d')
+# Get date of today
+date=$(/bin/date +'%Y%m%d')
 
 # Mountpoint for usb-backup-drive
 mpt="/volumeUSB3/usbshare/"
@@ -19,7 +19,7 @@ mpt="/volumeUSB3/usbshare/"
 # File to test, if the correct usb-drive was mounted
 test_file="001.mnt"
 
-# Logfile for the complete Backup
+# Logfile for the Logging Events at Backup
 LOGFILE="/volume1/PrivaterShare/logs/backup.log"
 
 # Logfile especially for rsync-tasks
@@ -30,11 +30,11 @@ RSYNC="/usr/syno/bin/rsync"
 
 # Control, if test_file is available
 if [ ! -f ${mpt}/${test_file} ] ; then
-    echo "$(/bin/date +'%Y-%m-%d_%H:%M:%S') - Backup-Drive   ${mpt} is not mounted" | tee -a $LOGFILE
+    echo "$(/bin/date +'%Y-%m-%d_%H:%M:%S') - The Backup-Drive was not mounted at the expected Mountpoint: ${mpt} " | tee -a $LOGFILE
     exit 1
 fi
 
-# Control if already backed-up today. If so, exit
+# Control if already backed up today. If so, exit
 CHECK_RSYNC=$(ps | grep -v grep | grep $RSYNC | wc -l)
 [ ${CHECK_RSYNC} -ne 0 ] && { echo "rsync am Laufen" ; exit 1 ; }
 
@@ -45,16 +45,16 @@ important_dirs="backup_list.txt"
 # Backup all important dirs to backup-mpt
 # for backup use rsync
 # Warning! All deleted files at nas will also be deleted at usb drive
-for zeile in $(grep -v "#" $important_dirs); do
-    mkdir -p ${mpt}/backups/${zeile}
-    if [ -e ${mpt}/backups/${zeile}/${datum}.bak ]; then
-        echo "Backup fuer ${zeile} heute bereits gelaufen"
+for line in $(grep -v "#" $important_dirs); do
+    mkdir -p ${mpt}/backups/${line}
+    if [ -e ${mpt}/backups/${line}/${date}.bak ]; then
+        echo "Already backed up the following directory or file today : ${line}"
         continue
     else
-        $RSYNC -av --delete --log-file="${LOGFILE_RSYNC}" -r ${zeile} ${mpt} 
-        touch ${mpt}/backups/${zeile}/${datum}.bak
-        echo "$(/bin/date +'%Y-%m-%d_%H:%M:%S') - rsync für folg. Verzeichnis erfolgreich gelaufen: $zeile" | tee -a $LOGFILE
-        echo "Details siehe ${LOGFILE_RSYNC}" | tee -a $LOGFILE
+        $RSYNC -av --delete --log-file="${LOGFILE_RSYNC}" -r ${line} ${mpt} 
+        touch ${mpt}/backups/${line}/${date}.bak
+        echo "$(/bin/date +'%Y-%m-%d_%H:%M:%S') - rsync for the following file or directy was successful: $line" | tee -a $LOGFILE
+        echo "For Details see: ${LOGFILE_RSYNC}" | tee -a $LOGFILE
     fi
 done
 
